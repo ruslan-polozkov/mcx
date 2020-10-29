c7e90eea53c0bc4e5cc58247988f5c48fc1d4b9d
r.palazkou <r.palazkou>
2020-10-29 18:34:52 +0300

    13

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "../styles/layout.scss"
+import Config from "../helpers/config";
 import Head from 'next/head'
+// import SmartBanner from ;
+import AppBanner from 'react-smart-app-banner';
+import NextNprogress from 'nextjs-progressbar';
 
 export default function MyApp({ Component, pageProps }) {
     return (
@@ -7,7 +11,27 @@ export default function MyApp({ Component, pageProps }) {
           <Head>
             <title>Dominoes</title>
           </Head>
+          <>
+            {/*<SmartBanner />*/}
+            <AppBanner
+              appInfo={Config}
+              position="top"
+              onClose={() => {
+                console.log('onClose called');
+              }}
+              hidden={false}
+              style={{
+                zIndex: 9999,
+              }}
+            />
+            <NextNprogress
+              color="#4aac00"
+              startPosition="0.3"
+              stopDelayMs="200"
+              height="5"
+            />
             <Component {...pageProps} />
+            </>
         </>
     )
 }
