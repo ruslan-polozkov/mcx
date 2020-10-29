5102305b2f612627b18884016837d259b89dad0c
r.palazkou <r.palazkou>
2020-10-29 22:31:49 +0300

    35

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
                     <meta name="title" content="Dominoes game" />
                     <meta name="description" content="Dominoes is a popular board game in which players line up a chain of dice that meet in halves with the same number of points." />
                     <meta name="keywords" content="dominoes,game,online,domino,best game" />
-                    <meta property="og:image" content="/public/img/phone-domino.png" />
+                    <meta property="og:image" content="img/phone-domino.png" />
                     <meta name="theme-color" content="#4aac00" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
                     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
