3a8cfea779514f5b028c57717a40c50427faee04
r.palazkou <r.palazkou>
2020-10-29 18:12:14 +0300

    twelve

--- pages/_document.js
+++ pages/_document.js
@@ -16,25 +16,21 @@ class MyDocument extends Document {
                     <meta name="keywords" content="dominoes,game,online,domino,best game" />
                     <meta property="og:image" content="/public/img/phone-domino.png" />
                     <meta name="theme-color" content="#4aac00" />
-                    <meta name="apple-itunes-app" content="app-id=1434957889, affiliate-data=myAffiliateData, app-argument=dominoes.com" />
-                    <meta name="google-play-app" content="app-id=loppipoppi.dominoes&hl=en" />
+                    {/*<meta name="apple-itunes-app" content="app-id=1434957889, affiliate-data=myAffiliateData, app-argument=dominoes.com" />*/}
+                    {/*<meta name="google-play-app" content="app-id=loppipoppi.dominoes&hl=en" />*/}
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
                     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
                     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
 
 
-                  <meta name="apple-itunes-app" content="app-id=502838820" />
-                    <meta name="google-play-app" content="app-id=ru.hh.android" />
-                      <meta name="msApplication-ID" content="microsoft.build.App" />
-                        <meta name="msApplication-PackageFamilyName" content="microsoft.build_8wekyb3d8bbwe" />
-                            <link rel="apple-touch-icon" href="/public/img/favicon.icon" />
-                              <link rel="android-touch-icon" href="/public/img/favicon.icon" />
-                                <link rel="windows-touch-icon" href="/public/img/favicon.icon" />
-                  <script src="/helpers/smart-app-banner.js"></script>
+                    <meta name="apple-itunes-app" content="app-id=1434957889, affiliate-data=myAffiliateData, app-argument=dominoes.com" />
+                    <meta name="google-play-app" content="app-id=loppipoppi.dominoes&hl=en" />
+                    <script src="/helpers/smart-app-banner.js" />
                 </Head>
                 <body>
                 <Main />
                 <NextScript />
+                <script src="/helpers/smart-app-banner.js" />
                 </body>
             </Html>
         )
